3f145f80a724d9f3eec5fb5c722dc31c40d527fb
Carter-eng <52756780+Carter-eng@users.noreply.github.com>
2021-07-13 16:35:05 -0400

    Add files via upload

--- Robot_Dashboard.py
+++ Robot_Dashboard.py
@@ -16,8 +16,7 @@ import numpy as np
 class RobotDashboard:
     def __init__(self):
         self.HOST = '192.168.1.180'
-        self.PORT = 5000
-
+        self.PORT = 4008
         self.server = socket.socket()
         self.server.bind((self.HOST,self.PORT))
         self.server.listen()
@@ -27,16 +26,17 @@ class RobotDashboard:
         self.listenerThread.start()
         self.robotNames = []
         self.robotAddresses = []
-
+        self.errorCounter = []
     def listener(self):
         while True:
             self.conn, self.address = self.server.accept()
-            self.robotNames.append(self.getname)
+            self.robotNames.append(self.getname())
             self.robotAddresses.append(self.address[0])
             self.conn.close()
-            self.ping.append(ping(self.address[0]))
             robotNumber = self.robotCounter
+            self.ping.append(ping(self.address[0]))
             threading.Thread(target=self.pinger, args=(robotNumber,)).start()
+            self.errorCounter.append(0)
             self.robotCounter += 1
     def pinger(self,robotNumber):
         while True:
@@ -44,14 +44,18 @@ class RobotDashboard:
             sleep(0.4)
     def generate_table(self) -> Table:
         table = Table(title="")
-        table.add_column("NAME", style="cyan")
-        table.add_column("IP", style="magenta")
-        table.add_column("PING", style="red")
-        table.add_column("POSE OPTITRACK", style="green")
-        table.add_column("POSE LOCAL ESTIMEATE", style="blue")
+        table.add_column("NAME")
+        table.add_column("IP")
+        table.add_column("PING")
+        table.add_column("POSE OPTITRACK")
+        table.add_column("POSE LOCAL ESTIMEATE")
         for i in range(self.robotCounter):
             if self.ping[i].rtt_avg_ms != 2000.0:
-                table.add_row(self.robotNames[i], str(self.robotAddresses[i]),str(self.ping[i].rtt_avg_ms)+" ms", "X: 0, Y:0, Z:0","X: 0, Y:0, Z:0")
+                self.errorCounter[i] = 0
+                table.add_row(str("[cyan]"+self.robotNames[i])+"[/cyan]","[magenta]" +str(self.robotAddresses[i])+"[/magenta]","[red]"+str(self.ping[i].rtt_avg_ms)+ " ms" +"[/red]","[green]X: 0, Y:0, Z:0[/green]","[blue]X: 0, Y:0, Z:0[/blue]")
+            elif self.errorCounter[i] < 100:
+                table.add_row("[gray]" + str(self.robotNames[i]) + "[/gray]", "[gray]"+str(self.robotAddresses[i])+"[/gray]","[gray]Robot Not Found[/gray]", "[gray]X: 0, Y:0, Z:0[/gray]","[gray]X: 0, Y:0, Z:0[/gray]")
+                self.errorCounter[i] +=1
             else:
                 pass
         return table
