221d183a6239e5490f2d38cea76a4993aa76bd04
Carter-eng <52756780+Carter-eng@users.noreply.github.com>
2022-02-08 12:22:30 -0500

    Update dashboard_plugins.py

--- dashboard_plugins.py
+++ dashboard_plugins.py
@@ -19,10 +19,27 @@ class Dashboard_Pose(robotAddresses,robotNumber,robotNames,robotType):
 		else:
 			pass
 	def subscriber(self,robotNames,robotNumber):
-        topicName = "/"+str(robotNames[robotNumber])+"/pose"
-        rospy.Subscriber(topicName,PoseStamped,self.poseUpdate,(robotNumber))
-        rospy.spin()
+        	topicName = "/"+str(robotNames[robotNumber])+"/pose"
+        	rospy.Subscriber(topicName,PoseStamped,self.poseUpdate,(robotNumber))
+        	rospy.spin()
 
-    def poseUpdate(self,msg,args):
-        position = msg.pose.position
-        self.value = "X:%f, Y:%f, Z:%F"%(position.x, position.y, position.z)
+   	def poseUpdate(self,msg,args):
+        	position = msg.pose.position
+        	self.value = "X:%f, Y:%f, Z:%F"%(position.x, position.y, position.z)
+
+class Dashboard_Battery(robotAddresses,robotNumber,robotNames):
+	def __init__(self):
+		self.label = "Battery Level"
+		self.value = ''
+		if robotAddresses !=0:
+			self.subscriber(robotNames,robotNumber)
+		else:
+			pass
+	def subscriber(self,robotNames,robotNumber):
+        	topicName = "/"+str(robotNames[robotNumber])+"/battery/charge_ratio"
+        	rospy.Subscriber(topicName,Float64,self.batteryUpdate)
+        	rospy.spin()
+
+    def batteryUpdate(self,battery):
+		Battery_Percent = battery.data*100
+		self.value = str(round(Battery_Percent,2))+"%"
