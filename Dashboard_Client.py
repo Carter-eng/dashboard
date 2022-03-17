1de2b161d874b1c4d08309e07f7d8152819f430a
Carter-eng <52756780+Carter-eng@users.noreply.github.com>
2022-03-17 15:51:10 -0400

    Update Dashboard_Client.py

--- Dashboard_Client.py
+++ Dashboard_Client.py
@@ -4,7 +4,7 @@ import socket
 
 def client_program():
     host = '192.168.1.170'
-    port = 40006
+    port = 40040
     client = socket.socket()
     client.connect((host, port))
     message = str(socket.gethostname()) + "^Drone"
